10bc56767fca66305ddeac601d4e31d692d4e2ad
Sean Wilson <sean.wilson@sheffield.ac.uk>
2018-08-16 15:52:53 +0100

    Split to own project

---
+++ Assets/Image Projector/Example/ImageProjectionExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ImageProjectionExample : MonoBehaviour {
+
+    public GameObject prefab;
+    public Texture testImage;
+
+    private GameObject projector;
+
+	void Start () {
+        // 1) Create an instance of the image projector and set location and rotation of projector
+        projector = Instantiate(prefab, new Vector3(-0.315f, 0.306f, 0.228f), Quaternion.Euler(2.711f, 161.066f, -0.918f));
+
+        // 2) Setup the projector to replicate camera view
+        projector.GetComponent<ImageProjector>().SetupProjector(55, testImage);
+    }
+}
---
+++ Assets/Image Projector/Scripts/ImageProjector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ImageProjector : MonoBehaviour {
+
+    Projector projector;
+    Material baseMat;
+    Texture image;
+
+	// Use this for initialization
+	void Awake () {
+        projector = GetComponent<Projector>();
+        baseMat = projector.material;
+	}
+
+    public void SetupProjector(float fov, Texture image)
+    {
+        projector.fieldOfView = fov;
+
+        this.image = image;
+        image.wrapMode = TextureWrapMode.Clamp;
+
+        projector.material = new Material(baseMat);
+        projector.material.SetTexture("_ShadowTex", image);
+    }
+}
